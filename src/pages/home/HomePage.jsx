48931037baa47eee8ba02f9c12c0882de52ecfc4
Kim Bucado <bucado.kim@gmail.com>
2026-04-29 22:35:23 -0400

    Modified Section Layout

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Container from "../layout/Container";
 import CarouselComponent from "@/ui components/carousel/CarouselComponent";
 import PropTypes from "prop-types";
+import Section from "../layout/Section";
 
 const defaultHomePictures = [
   "https://www.floridastateparks.org/sites/default/files/styles/gallery/public/media/image/30741024_Wekiwa%20Springs%20State%20Park_New%20Perspective_Perfect%20Day%20for%20a%20Paddle_Becky_Johns%20Witek.jpg?itok=qILfEvsg",
@@ -13,6 +14,11 @@ const HomePage = ({ images = defaultHomePictures }) => {
   return (
     <Container containerStyles="py-5 px-4">
       <CarouselComponent images={images} />
+      <Section
+        title="Featured Destinations"
+        subtitle="Handpicked spots across Florida you won't find in typical guides."
+        containerStyles="py-5 px-4"
+      ></Section>
     </Container>
   );
 };
